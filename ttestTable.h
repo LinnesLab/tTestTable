55de56696f855938ea2f04c231d76de8c6bf071e
Orlando Hoilett <orlandohoilett@Orlandos-MacBook-Pro-9016.local>
2020-07-13 00:37:19 -0400

    initial release

---
+++ ttestTable.h
@@ -0,0 +1,23 @@
+//
+//  ttestTable.h
+//  
+//
+//  Created by Orlando Hoilett on 3/5/20.
+//
+
+#ifndef ttestTable_h
+#define ttestTable_h
+
+
+#include <Arduino.h>
+
+
+//https://en.wikipedia.org/wiki/Student%27s_t-distribution#Table_of_selected_values
+//two-tailed, 95% confidence interval
+const float a95[] = {0, 12.71, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365, 2.306, 2.262,
+	2.228, 2.201, 2.179, 2.16, 2.145, 2.131, 2.12, 2.11, 2.101, 2.093, 2.086,
+	2.08, 2.074, 2.069, 2.064, 2.06, 2.056, 2.052, 2.048, 2.045, 2.042, 2.021,
+	2.009, 2, 1.99, 1.984, 1.98 };
+
+
+#endif /* ttestTable_h */
